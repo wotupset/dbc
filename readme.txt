03ad984d84fd4fc7d55eb60240dc5048cfd29b85
wotupset <wotupset@gmail.com>
2013-07-24 10:18:21 +0800

    更名

--- readme.txt
+++
@@ -1,3 +0,0 @@
-rem php + mysql 的臨時簡易留言板
-rem ㄏㄏ
-set HOME="D:\ftprush_2.1.8\My FTPRush Downloads\dbc\.git"
---
+++ readme.txt.bat
@@ -0,0 +1,5 @@
+rem php + mysql 的臨時簡易留言板
+rem ㄏㄏ
+rem %HOMEDRIVE%
+@if not exist "%HOME%" @set HOME=%HOMEPATH%
+@if not exist "%HOME%" @set HOME=%USERPROFILE%
